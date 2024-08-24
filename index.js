274122df8d0b85fd4fd59f535b4d0fa6a6b05663
Zakria Younas <zakriayounas01@gmail.com>
2024-08-24 12:19:08 +0500

    added cors middleware

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import connectDB from './database/connect.js';
 import userRouter from "./routes/userRoutes.js"
 import teacherRouter from './routes/teacherRoutes.js';
 import studentRouter from './routes/studentRoutes.js';
+import cors from "cors"
 // Load environment variables
 dotenv.config();
 
@@ -15,6 +16,8 @@ const app = express();
 // Body parser middleware
 app.use(express.json());
 
+// Use CORS middleware
+app.use(cors());
 // Use routes
 app.use('/api/user', userRouter);
 app.use('/api/teachers', teacherRouter);
