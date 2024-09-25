42cf9471d97247e705101fb195bb13bc3cdf2c44
Zakria Younas <zakriayounas01@gmail.com>
2024-09-25 10:47:26 +0500

    added code for s3 configuration

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -26,3 +26,22 @@ export const uploadFile = multer({
     limits: { fileSize: 1000000 }, //  size limit to 1MB
     fileFilter: filterFileType,
 });
+
+
+// AWS S3
+// import multerS3 from "multer-s3"
+// import AWS from "aws-sdk"
+// const s3 = new AWS.S3({
+//     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+//     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+//     region: process.env.AWS_REGION
+// });
+// const fileStorage = multerS3({
+//     s3: s3,
+//     bucket: process.env.S3_BUCKET_NAME, // Your S3 bucket name
+//     acl: 'public-read', // Set permissions (can be 'private', 'public-read', etc.)
+//     contentType: multerS3.AUTO_CONTENT_TYPE, // Automatically set the content type
+//     key: (req, file, cb) => {
+//         cb(null, `uploads/${Date.now()}-${file.originalname}`); // Define the path in the S3 bucket
+//     }
+// })
