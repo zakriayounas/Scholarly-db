c3daae93d31c5483a0de62f4717fd926ff7e5aae
Zakria Younas <zakriayounas01@gmail.com>
2024-09-25 08:54:41 +0500

    minor change

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -3,7 +3,7 @@ import path from 'path';
 
 const fileStorage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, "./uploads/profileImages"); // Save to the 'uploads/profile_images' directory
+        cb(null, "../uploads/profileImages"); // Save to the 'uploads/profileImages' directory
     },
     filename: (req, file, cb) => {
         cb(null, `${Date.now()}-${file.originalname}`);
